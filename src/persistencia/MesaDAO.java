b3e184225363e730e04b2cab862ea75adfad9306
unknown <samuelnunez@uninorte.edu.co>
2026-03-01 18:24:41 -0500

    mesaDAO con todos los metodos necesarios

--- src/persistencia/MesaDAO.java
+++ src/persistencia/MesaDAO.java
@@ -44,4 +44,61 @@ public class MesaDAO {
 
         return lista;
     }
+    
+    public void guardarTodas(ArrayList<Mesa> lista) {
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(fileName));
+            for (Mesa m : lista) {
+                bw.write(m.getIdMesa() + "," + m.getCapacidad() + "," + m.getEstado());
+                bw.newLine();
+            }
+            bw.close();
+        } catch (Exception e) {
+            System.out.println("Error al guardar: " + e.getMessage());
+        }
+    }
+    
+    //boton añadir mesa
+    public void agregarMesa(Mesa nueva) {
+        ArrayList<Mesa> lista = cargarMesas();
+        for(Mesa m : lista) {
+            if (m.getIdMesa() == nueva.getIdMesa()) {
+                System.out.println("Error, ID dulicado");
+                return;
+            }
+        }
+        lista.add(nueva);
+        guardarTodas(lista);
+    }
+    
+    //boton de editar mesa
+    public void editarMesa(Mesa editada) {
+        ArrayList<Mesa> lista = cargarMesas();
+        for (int i = 0; i < lista.size(); i++) {
+            if (lista.get(i).getIdMesa() == editada.getIdMesa()) {
+                lista.set(i, editada);
+                break;
+            }
+        }
+        guardarTodas(lista);
+    }
+    
+    //boton de eliminar mesa
+    public void eliminarMesa(int idMesa) {
+        ArrayList<Mesa> lista = cargarMesas();
+        lista.removeIf(m -> m.getIdMesa() == idMesa);
+        guardarTodas(lista);
+    }
+    
+    //boton para cambiar estado
+    public void cambiarEstado(int idMesa, String nuevoEstado) {
+        ArrayList<Mesa> lista = cargarMesas();
+        for (Mesa m : lista) {
+            if(m.getIdMesa() == idMesa) {
+                m.setEstado(nuevoEstado);
+                break;
+            }
+        }
+        guardarTodas(lista);
+    }
 }
